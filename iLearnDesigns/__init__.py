e4a7a88e821f98bd86df349184dcb1a5f1aef18d
SolidShrek <shrekwang592@gmail.com>
2024-04-17 11:44:02 -0700

    Update __init__.py

--- iLearnDesigns/__init__.py
+++ iLearnDesigns/__init__.py
@@ -1,1 +1,2 @@
 # Designed by iLearn, I just helped to implement/debug
+# https://github.com/lexmakali/system-design-primer?tab=readme-ov-file#object-oriented-design-interview-questions-with-solutions
